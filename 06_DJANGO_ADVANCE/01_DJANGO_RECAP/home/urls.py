4cf40a0bea3fa4c0ba58d9aa569c94541309f303
hagi <ceajer2002@naver.com>
2019-10-04 16:40:59 +0900

    django prac

---
+++ 06_DJANGO_ADVANCE/01_DJANGO_RECAP/home/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'home'
+
+urlpatterns = [
+    path('guess/', views.guess, name='guess'),  # HOST/home/hi/
+    path('', views.index, name='index'),  # HOST/home/ 아무것도 없기 때문에
+    path('answer/', views.answer, name='answer')
+]
---
+++ 06_DJANGO_ADVANCE/01_DJANGO_RECAP/home/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def guess(request):
+    return render(request, 'home/guess.html')
+
+
+def index(request):
+    return render(request, 'home/index.html')
+
+
+def answer(request):
+    count = 0
+    if request.GET.get('q1') == '0115':
+        count += 1
+    if request.GET.get('q2') == '275':
+        count += 1
+    if request.GET.get('q3') == 'B':
+        count += 1
+    return render(request, 'home/answer.html', {'count':count})
