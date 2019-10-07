f66779e66d8c502def7563de96cfac18adf19445
hagi <ceajer2002@naver.com>
2019-10-07 13:04:20 +0900

    make model Article

--- 06_DJANGO_ADVANCE/01_DJANGO_RECAP/django_recap/settings.py
+++ 06_DJANGO_ADVANCE/01_DJANGO_RECAP/django_recap/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_extensions', # 이건 반드시 _ 써줘야한다.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
