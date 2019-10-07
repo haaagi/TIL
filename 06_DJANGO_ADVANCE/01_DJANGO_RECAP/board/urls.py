c7b39ee6ae83048ed99019d13fa617bc8649e9de
hagi <ceajer2002@naver.com>
2019-10-07 13:55:01 +0900

    implement read with MTV

--- 06_DJANGO_ADVANCE/01_DJANGO_RECAP/board/urls.py
+++ 06_DJANGO_ADVANCE/01_DJANGO_RECAP/board/urls.py
@@ -4,5 +4,16 @@ from . import views
 app_name = 'board'
 
 urlpatterns = [
-    path('', views.index, name = 'index'), # /board/
+    path('', views.index, name ='index'), # /board/
+    # Read 글 목록  render
+    path('articles/', views.list, name='list'),
+    # Read글 상세 render
+    path('articles/<int:id>/', views.detail, name='detail')
+    # Create 글 쓰기 render
+    # Create글 저장
+
+    # Update 글 수정 render
+    # Update 글 실제수정
+
+    # Delete 글 삭제
 ]
--- 06_DJANGO_ADVANCE/01_DJANGO_RECAP/board/views.py
+++ 06_DJANGO_ADVANCE/01_DJANGO_RECAP/board/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 
 
 def index(request):
     return render(request, 'board/index.html')
-    
+
+
+def list(request):
+    articles = Article.objects.all()   # [<A1>,<A2>,<A3>,....] 이런식으로 들어옴
+    return render(request, 'board/list.html', {
+        'articles':articles,
+    })
+
+
+def detail(request, id):
+    article = Article.objects.get(id=id)
+    return render(request, 'board/detail.html', {
+        'article':article,
+    })
+
+
